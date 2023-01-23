6b811eee94772522167783548f82ae25ca7119bb
PedroVisconti <pedroarthurvisconti@hotmail.com>
2023-01-23 16:58:07 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Nome);
+            Console.WriteLine(filme.Duração);
+        }
+
+
+
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public int Duração { get; set; }
+
+    }
+}
